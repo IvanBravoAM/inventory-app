d72981e8fe70f86f8dbd0a287200f58767796adf
Ivan Bravo <ivanbravo.2201@gmail.com>
2023-08-13 16:57:06 -0300

    missing changes

--- ProductManager/public/js/index.js
+++ ProductManager/public/js/index.js
@@ -2,46 +2,71 @@ const socket = io();
 
 socket.emit("connection", "new client connection");
 
-document.getElementById("productForm").addEventListener("submit", (event) => {
+// document.getElementById("productForm").addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+//     const productCategory = document.getElementById("productCategory").value;
+//     const productTitle = document.getElementById("productTitle").value;
+//     const productDescription = document.getElementById("productDescription").value;
+//     const productPrice = document.getElementById("productPrice").value;
+//     const productThumbnail = document.getElementById("productThumbnail").value;
+//     const productStock = document.getElementById("productStock").value;
+//     const productCode = document.getElementById("productCode").value;
+
+//     socket.emit("addProduct", {
+//         category: productCategory,
+//         title: productTitle,
+//         description: productDescription,
+//         price: productPrice,
+//         thumbnail: productThumbnail,
+//         stock: productStock,
+//         code: productCode
+//     });
+//     document.getElementById("productCategory").value = "";
+//     location.reload();
+// });
+
+
+// // Agregar un nuevo producto a la lista
+// socket.on("newProductAdded", (newProduct) => {
+//     const productList = document.getElementById("productList");
+//     const li = document.createElement("li");
+//     li.textContent = newProduct;
+//     productList.appendChild(li);
+// });
+
+// //delete button func
+// const btnEliminar = li.querySelector(".btnEliminar");
+// btnEliminar.addEventListener("click", () => {
+//     deleteProduct(product.id);
+// });
+
+// //delete function
+// function deleteProduct(productID) {
+//     socket.emit("deleteProduct", productID);
+// }
+
+//events chat
+const messageForm = document.getElementById('messageForm');
+messageForm.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    const productCategory = document.getElementById("productCategory").value;
-    const productTitle = document.getElementById("productTitle").value;
-    const productDescription = document.getElementById("productDescription").value;
-    const productPrice = document.getElementById("productPrice").value;
-    const productThumbnail = document.getElementById("productThumbnail").value;
-    const productStock = document.getElementById("productStock").value;
-    const productCode = document.getElementById("productCode").value;
-
-    socket.emit("addProduct", {
-        category: productCategory,
-        title: productTitle,
-        description: productDescription,
-        price: productPrice,
-        thumbnail: productThumbnail,
-        stock: productStock,
-        code: productCode
+    const user = userInput.value;
+    const message = messageInput.value;
+    if (user && message) {
+        const messageObject = { user, message };
+        socket.emit('addMessage', messageObject);
+        messageInput.value = '';
+    }
     });
-    document.getElementById("productCategory").value = "";
-    location.reload();
-});
 
-
-// Agregar un nuevo producto a la lista
-socket.on("newProductAdded", (newProduct) => {
-    const productList = document.getElementById("productList");
+socket.on("newMessage", chat => {
+    const {user,message} = chat;
+    const messageList = document.getElementById('messagesList');
     const li = document.createElement("li");
-    li.textContent = newProduct;
-    productList.appendChild(li);
-});
-
-//delete button func
-const btnEliminar = li.querySelector(".btnEliminar");
-btnEliminar.addEventListener("click", () => {
-    deleteProduct(product.id);
-});
-
-//delete function
-function deleteProduct(productID) {
-    socket.emit("deleteProduct", productID);
-}
+    const h3 = document.createElement("h3");
+    li.textContent = message;
+    h3.textContent = user;
+    console.log('asdas'+user+message);
+    messageList.appendChild(h3);
+    messageList.appendChild(li);
+});    
